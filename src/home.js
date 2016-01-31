4fe2ca643d1496aebf1c5da50e44c1dd7678805e
Rahmat Aligos <artasikindonesia@gmail.com>
2016-01-31 18:28:52 +0700

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,17 +1,17 @@
 let Home = React.createClass({
     render: function () {
         var socials = [
-            {link: 'https://github.com/sonnylazuardi', icon: 'github'},
-            {link: 'http://linkedin.com/in/sonnylazuardi', icon: 'linkedin'},
-            {link: 'https://speakerdeck.com/sonnylazuardi', icon: 'slideshare'},
-            {link: 'http://twitter.com/sonnylazuardi', icon: 'twitter'},
-            {link: 'http://facebook.com/sonny.lazuardi', icon: 'facebook'},
+            {link: 'https://github.com/aligos', icon: 'github'},
+            {link: 'http://linkedin.com/in/aligos', icon: 'linkedin'},
+            {link: 'https://speakerdeck.com/aligos', icon: 'slideshare'},
+            {link: 'http://twitter.com/aligos', icon: 'twitter'},
+            {link: 'http://facebook.com/matt.aligos', icon: 'facebook'},
         ]
         return (
             <div className="about">
                 <img src="img/header.png"/>
-                <h1 className="jumbo-title">I create web & mobile apps.</h1>
-                <p className="jumbo-desc">Sonny Lazuardi - a web & mobile developer, open source contributor, Javascript fanboy</p>
+                <h1 className="jumbo-title">I create web apps & mobile apps.</h1>
+                <p className="jumbo-desc">Rahmat ALigos JavaScript Fans</p>
 
                 <div className="social">
                     {socials.map((item) => (<a href={item.link}><i className={`fa fa-${item.icon}`}></i></a>))}
